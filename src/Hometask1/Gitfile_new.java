99b8398b50698a94885dbff016ffb6dd8bd3efc4
ankit <ankit@DESKTOP-A7CGO0P>
2024-06-11 20:51:21 +0530

    A213/Comitting anotherfile

---
+++ src/Hometask1/Gitfile_new.java
@@ -0,0 +1,20 @@
+package Hometask1;
+
+public class Gitfile_new {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=0;
+		int c=1;
+		for(int i=1; i<=10; i++) {
+		System.out.println(b); //0
+		int sum = b+c; //0+1=1
+		b=c; //
+		b=sum;
+		
+
+	}
+
+}
+
+}
